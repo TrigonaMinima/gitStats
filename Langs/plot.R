b487b27e70bee6ee15da8ccbe533d1cf8f0c4356
TrigonaMinima <shivamrana95@yahoo.in>
2015-04-11 15:04:15 +0530

    Generating the plot.

---
+++ Langs/plot.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+
+langs = read.csv("langs.csv")
+
+langs$bytes <- langs$bytes/80
+langs <- transform(langs, lang = reorder(lang, bytes))
+langs <- subset(langs, langs$bytes > 100)
+
+
+g <- ggplot(langs, aes(x=lang, y=bytes, fill=lang)) +
+    geom_bar(colour="black", stat="identity") +
+    guides(fill=F) +
+    labs(x="", y="Lines of Code") +
+    scale_fill_brewer() +
+    coord_flip()
+
+
+print(g)
