71099aac9f2b6a46524139c111729d99c3255f7a
TrigonaMinima <shivamrana95@yahoo.in>
2015-04-11 15:03:49 +0530

    Getting the Data.

---
+++ Langs/lang_stat.py
@@ -0,0 +1,37 @@
+# import json
+from github import Github as git
+import csv
+
+g = git("Username", "Password")
+
+lang_code = {}
+
+for repo in g.get_user().get_repos():
+    # print(json.dumps(repo.raw_data, separators=(',', ':'), indent=4))
+    # print("1", repo.name)
+    # print("2", repo.description)
+    # print("3", repo.language)
+    # print("4", repo.size)
+    # print("5", repo.get_languages())
+    # print()
+
+    langs = repo.get_languages()
+
+    for lang in langs:
+        if lang in lang_code:
+            lang_code[lang] += langs[lang]
+        else:
+            lang_code[lang] = langs[lang]
+
+
+lang_code["Python"] -= 23746
+
+for lang in ["PHP", "CSS", "JavaScript"]:
+    lang_code.pop(lang)
+
+with open("langs.csv", "w", newline="") as f:
+    write = csv.writer(
+        f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    write.writerow(["lang", "bytes"])
+    for lang in lang_code:
+        write.writerow([lang, lang_code[lang]])
