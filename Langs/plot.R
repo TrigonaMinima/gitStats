ce2574b851d273c4a6f9202036918093f17356e4
TrigonaMinima <shivamrana95@yahoo.in>
2015-04-11 15:39:01 +0530

    Minor changes.

--- Langs/plot.R
+++ Langs/plot.R
@@ -7,12 +7,12 @@ langs <- transform(langs, lang = reorder(lang, bytes))
 langs <- subset(langs, langs$bytes > 100)
 
 
+png("lang.png", res=100)
 g <- ggplot(langs, aes(x=lang, y=bytes, fill=lang)) +
     geom_bar(colour="black", stat="identity") +
     guides(fill=F) +
     labs(x="", y="Lines of Code") +
     scale_fill_brewer() +
     coord_flip()
-
-
 print(g)
+dev.off()
